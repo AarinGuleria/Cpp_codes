1ce9368a9537296f3c07a530b4bdc0bfbd7e8d18
AarinGuleria <aaringuleria2310@gmail.com>
2024-08-24 11:15:26 +0530

    2d arrays start

---
+++ All programs/Arrays/2D_Arrays/problems_to_try/array_Multiplication_entered_by_user.cpp
@@ -0,0 +1,50 @@
+// Write a program to display multiplication 2 matrices entered by user.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int r1, c1;
+  cin >> r1 >> c1;
+
+  int A[r1][c1];
+  for (int i = 0; i < r1; i++) {
+    for (int j = 0; j < c1; j++) {
+      cin >> A[i][j];
+    }
+  }
+
+  int r2, c2;
+  cin >> r2 >> c2;
+
+  int B[r2][c2];
+  for (int i = 0; i < r2; i++) {
+    for (int j = 0; j < c2; j++) {
+      cin >> B[i][j];
+    }
+  }
+
+  if (c1 != r2) {
+    cout << "Multiplication not possible" << endl;
+  }
+
+  int C[r1][c2];
+  for (int i = 0; i < r1; i++) {
+    for (int j = 0; j < c2; j++) {
+      int value = 0;
+      for (int k = 0; k < c1 ; k++) { // k<r2 can also be used
+        value += A[i][k] * B[k][j];
+      }
+      C[i][j] = value;
+    }
+  }
+
+  // displaying the resultant C matrix
+  for (int i = 0; i < r1; i++) {
+    for (int j = 0; j < c2; j++) {
+      cout << C[i][j] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
---
+++ All programs/Arrays/2D_Arrays/problems_to_try/transpose_Of_matrix.cpp
@@ -0,0 +1,37 @@
+// Write a program to display transpose of a mtrix entered by the user.
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n,m;
+    cin>>n>>m;
+
+    int array[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>array[i][j];
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0;j<m;j++){
+            cout<<array[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    int tranpose_Array[m][n];
+    for(int i=0; i<m; i++){
+        for(int j=0;j<n;j++){
+            tranpose_Array[i][j] = array[j][i];
+        }
+    }
+
+    for(int i=0; i<m; i++){
+        for(int j=0;j<n;j++){
+            cout<<tranpose_Array[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
