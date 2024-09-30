e216d05117c603725535f2bf88d4d8074e246201
AarinGuleria <aaringuleria2310@gmail.com>
2024-09-30 20:50:37 +0530

    updation

--- All programs/Arrays/Array lecture/checkarraysortedornot.cpp
+++ All programs/Arrays/Array lecture/checkarraysortedornot.cpp
@@ -4,18 +4,18 @@
 #include<vector>
 using namespace std;
 
-int main(){
-
-    int array[6]={1,2,3,4,5,6};
+int main() {
+    int array[6] = {1, 2, 3, 4, 5, 6};
     
-    bool sortedflag=true;
-    for(int i=1; i<6; i++){
-        if(array[i]<array[i-1]){
-            sortedflag=false;
+    bool sortedflag = true;
+    for (int i = 1; i < 6; i++) {
+        if (array[i] < array[i - 1]) {
+            sortedflag = false;
+            break; // Exit the loop as soon as the array is found to be unsorted
         }
     }
 
-    cout<<sortedflag<<endl;
+    cout << sortedflag << endl; // Output: 1 (true) if sorted, 0 (false) if not sorted
 
     return 0;
 }
