9a060748c7568e5a6605c7eded09f3d79cbdf5fa
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-30 23:02:54 +0530

    Fix bubble sort implementation and correct output stream

--- TakeuForward/Sorting/BubbleSort.cpp
+++ TakeuForward/Sorting/BubbleSort.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bubble_sort(){
+void bubble_sort(int arr[], int n){
     for(int i=n-1; i>=0; i--){
         for(int j=0; j<=i-1; j++){
-            if(arr[j] < arr[j+1]){
-                swap(arr[j], arr[j+1]);
+            if(arr[j] > arr[j+1]){
+                swap(arr[j+1], arr[j]);
             }
         }
     }
@@ -22,7 +22,7 @@ int main(){
     }
     bubble_sort(arr,n);
     for(int i=0; i<n; i++){
-        cin<<arr[i]<<" ";
+        cout<<arr[i]<<" ";
     }
 
     return 0;
