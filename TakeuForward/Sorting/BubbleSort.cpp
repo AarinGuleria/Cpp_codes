462efb37bef2b8324f825d516bb3b8d2a20f2ea4
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-31 19:42:16 +0530

    Refactor bubble sort implementation for efficiency and improve code formatting

--- TakeuForward/Sorting/BubbleSort.cpp
+++ TakeuForward/Sorting/BubbleSort.cpp
@@ -1,29 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void bubble_sort(int arr[], int n){
-    for(int i=n-1; i>=0; i--){
-        for(int j=0; j<=i-1; j++){
-            if(arr[j] > arr[j+1]){
-                swap(arr[j+1], arr[j]);
-            }
-        }
+void bubble_sort(int arr[], int n) {
+  for (int i = n - 1; i >= 0; i--) {
+    int swapDid = 0;
+    for (int j = 0; j <= i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j + 1], arr[j]);
+        swapDid = 1;
+      }
     }
+    if (swapDid == 0) {
+      break;
+    }
+    cout << "runs\n";
+  }
 }
 
-int main(){
+int main() {
+  int n;
+  cin >> n;
 
-    int n;
-    cin>>n;
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  bubble_sort(arr, n);
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
 
-    int arr[n];
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-    bubble_sort(arr,n);
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
-
-    return 0;
+  return 0;
 }
